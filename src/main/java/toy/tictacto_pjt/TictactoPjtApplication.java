5ef29a9b3629479288ed4fae3d16248e1bbb5902
jh214 <sswed147@naver.com>
2025-01-10 17:36:19 +0900

    init : 프로젝트 생성

---
+++ src/main/java/toy/tictacto_pjt/TictactoPjtApplication.java
@@ -0,0 +1,13 @@
+package toy.tictacto_pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactoPjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/tictacto_pjt/TictactoPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.tictacto_pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
